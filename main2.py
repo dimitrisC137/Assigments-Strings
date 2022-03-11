366c968603ed2d7e2c5794640ebc0adb1a4c4f3b
dimitrisC137 <100574536+dimitrisC137@users.noreply.github.com>
2022-03-11 13:11:08 +0100

    Delete main2.py

--- main2.py
+++
@@ -1,8 +0,0 @@
-player = "Marco van Basten"
-first_name = player[0:5] #I dont know how to use slicing and the find-method to isolate and store the first name, can you please show me?
-last_name =  player[6:] 
-last_name_len = len(last_name)
-dot = ". "
-name_short = first_name[0] + dot + last_name
-chant = f" {first_name}!"
-print ((chant)*len(first_name))
